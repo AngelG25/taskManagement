20db10b09968ef7fc814258bafe7ab5cdbceca4f
Angel <angel.garcia.cortes25@gmail.com>
2025-01-30 19:46:54 +0100

    Documentation added and swagger documentation removed for now

--- src/main/java/com/portfolio/rest/TaskRest.java
+++ src/main/java/com/portfolio/rest/TaskRest.java
@@ -2,16 +2,11 @@ package com.portfolio.rest;
 
 import com.portfolio.api.TaskApi;
 import com.portfolio.api.models.Task;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.stream.Stream;
 
-@Tag(name = "Task")
 public class TaskRest implements TaskApi {
 
     @GET
