10c5b2db831c0b34b0aa31cbb679cadf1f87dcef
Angel <angel.garcia.cortes25@gmail.com>
2025-01-30 17:37:36 +0100

    Remove unnecessary dependencies and added explanation

--- src/main/java/com/portfolio/rest/TaskRest.java
+++ src/main/java/com/portfolio/rest/TaskRest.java
@@ -3,8 +3,7 @@ package com.portfolio.rest;
 import com.portfolio.api.TaskApi;
 import com.portfolio.api.models.Task;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,28 +14,32 @@ import java.util.stream.Stream;
 @Tag(name = "Task")
 public class TaskRest implements TaskApi {
 
-    @GetMapping("/getTasks")
+    @GET
+    @Path("/getTasks")
     @Produces(MediaType.APPLICATION_JSON)       // JAKARTA
     public Stream<Task> getTasks() {
         return Stream.empty();
     }
 
-    @GetMapping("/getTaskById/{idTask}")
+    @GET
+    @Path("/getTaskById/{idTask}")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.TEXT_PLAIN)
-    public Task getTaskById(@PathVariable("idTask") String idTask) {
+    public Task getTaskById(@PathParam("idTask") String idTask) {
         return null;
     }
 
-    @PostMapping("/updateTask")
+    @POST
+    @Path("/updateTask")
     @Consumes(MediaType.APPLICATION_JSON)
     public Boolean updateTask(Task task) {
         return null;
     }
 
-    @PostMapping("/deleteTask/{idTask}")
+    @POST
+    @Path("/deleteTask/{idTask}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Boolean removeTask(@PathVariable("idTask") String idTask) {
+    public Boolean removeTask(@PathParam("idTask") String idTask) {
         return null;
     }
 }
