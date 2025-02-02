5161b844c728b232045c835dc565e6f01ffbaee0
Angel <angel.garcia.cortes25@gmail.com>
2025-02-02 17:52:38 +0100

    Added taskDao in the repository and objectMapper

--- src/main/java/com/portfolio/srv/TaskSrv.java
+++ src/main/java/com/portfolio/srv/TaskSrv.java
@@ -1,8 +1,10 @@
 package com.portfolio.srv;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.portfolio.api.TaskApi;
 import com.portfolio.api.exceptions.TaskNotFoundException;
 import com.portfolio.api.models.Task;
+import com.portfolio.dao.TaskDao;
 import com.portfolio.repositories.TaskRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -19,20 +21,21 @@ public class TaskSrv implements TaskApi {
     private static final String TASK_ID = "Task with id: ";
     private static final String NOT_FOUND = "couldn't be found";
     private static final String LOG_ERROR = "Task with id: {} couldn't be found";
-
-    // TODO FIX all the repository to use TaskDao instead of Task
+    private ObjectMapper objectMapper;
 
     @Override
     public Stream<Task> getTasks() {
         log.info("Get tasks request..");
-        return taskRepository.findAll().stream();
+        return taskRepository.findAll().stream()
+                .map(taskDao -> objectMapper.convertValue(taskDao, Task.class));
     }
 
     @Override
     public Task getTaskById(String idTask) {
         log.info("Get task by id request...");
-        return taskRepository.findById(idTask)
+        final TaskDao taskDao = taskRepository.findById(idTask)
                 .orElseThrow(() -> new TaskNotFoundException(TASK_ID + idTask + NOT_FOUND));
+        return objectMapper.convertValue(taskDao, Task.class);
     }
 
     @Override
@@ -40,7 +43,7 @@ public class TaskSrv implements TaskApi {
         log.info("Update task request...");
         final String idTask = updatedTask.getIdTask();
         if (checkExistence(idTask)) {
-            taskRepository.save(updatedTask);
+            taskRepository.save(objectMapper.convertValue(updatedTask, TaskDao.class));
         } else {
             throwException(idTask);
         }
